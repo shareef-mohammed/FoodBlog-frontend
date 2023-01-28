b0bd0b2254bb52c8b351b6b5fc5b6cb1a9b0d87f
shareef <shareefmohammedmm@gmail.com>
2023-01-28 19:03:43 +0530

    error8

--- src/components/Location/LocateButton.jsx
+++ src/components/Location/LocateButton.jsx
@@ -29,8 +29,6 @@ const LocateButton = ({ page }) => {
     }
   }, [location]);
 
-  console.log(user)
-
   if (location)
     return (
       <Location
@@ -56,7 +54,7 @@ const LocateButton = ({ page }) => {
           onClick={() => setLocation(true)}
           class="relative  inline-flex items-center justify-center p-0.5 mb-2 mx-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
         >
-          {user.location ?<span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+          {user && user.location ?<span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
              <span className="uppercase font-semibold">{user.location}</span> (Change your location)</span> : 
              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">'Add location' 
           </span>}
