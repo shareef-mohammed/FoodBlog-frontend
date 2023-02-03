30cccf675e4f2705029e24302535ab9da7fd018c
shareef <shareefmohammedmm@gmail.com>
2023-02-03 11:00:17 +0530

    chat3

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -79,7 +79,7 @@ const Navba = ({ home, posts, profile, chats }) => {
     <Location opened={location} user={user} onClose={() => {
         setLocation(false)
       }} />
-      <div className="flex justify-between sticky top-0 left-0 right-0 z-10 items-center max-w-[1255px] mx-auto px-4 rounded-xl bg-gradient-to-r from-[#e7e5e4] to-[#fee2e2]">
+      <div className="flex justify-between sticky top-0 left-0 right-0 z-10 items-center max-w-screen mx-auto px-4 rounded-xl bg-gradient-to-r from-[#e7e5e4] to-[#fee2e2]">
         <div className="text-center">
           <img
             className="rounded-full mt-2 ms-3 w-24 h-16 cursor-pointer"
